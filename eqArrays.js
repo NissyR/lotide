0976310ca3d44fe7cb2fd8ee0e04ddf77c481cd6
Edara15 <89929601+NissyR@users.noreply.github.com>
2021-10-27 15:23:07 -0400

    implmented Head

--- eqArrays.js
+++ eqArrays.js
@@ -4,3 +4,5 @@ const assertEqual = (actual, expected) => {
 };
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+// eslint-disable-next-line no-undef
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
